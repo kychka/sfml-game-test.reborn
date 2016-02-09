f26e6b506a200d08b8480c6a154b05010a08f8ed
Alexey <alekseydavaydosvidaniya@gmail.com>
2016-02-10 09:36:59 +1000

    Test menuScreen  НУЖНО создать базовый класс аbstractScreen и менеджер к нему. для абстрактного класса необходимо выяснить что будет присутствовать в любом из окон

--- main.cpp
+++ main.cpp
@@ -1,25 +1,18 @@
 #include <SFML/Graphics.hpp>
-
-
+#include "MainGameClass/Game.h"
+#include "Screens/StartMenu/StartMenu.h"
 int main()
 {
-
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
-	 sf::Event event;
+	
+	/*new Game().run();*/
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+			   // все ,что ниже - только для теста
+	startMenu menu(window);
 	while (window.isOpen())
 	{
-		
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-
-		window.clear();
-		window.draw(shape);
-		window.display();  
+			
+		menu.menuControl();
+		menu.runMenu();
 	}							  
 
 	return 0;
