b68cd85f750fca0aca3d8126390d7598999cd096
Alexey <alekseydavaydosvidaniya@gmail.com>
2016-02-12 23:48:39 +1000

    modified main.cpp

--- main.cpp
+++ main.cpp
@@ -2,21 +2,21 @@
 #include <SFML/System.hpp>
 #include <iostream>
 #include "MainGameClass/Game.h"
-<<<<<<< HEAD
-#include "Screens/StartMenu/StartMenu.h"
+
+#include "Screens/Menu/StartMenu.h"
 #include "Animation/IAnimation.h"
 #include "Animation/AnimationClass.h"
 using namespace sf;
-=======
-#include "Screens/Menu/StartMenu.h"
+
+
 #include "Screens/Menu/pauseMenu.h"
->>>>>>> master
+
 int main()
 {
 				  
 	/* new Game().run(); */		
 	
-<<<<<<< HEAD
+
 	// все ,что ниже - только для теста 
 	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
 	Texture texture;
@@ -28,51 +28,34 @@ int main()
 
 	 window.setFramerateLimit(60);
 	IAnimation *ExplosionAnimation = new AnimationClass(texture, 4, 4, 2000.f, 0, 0, 64, 64);
-//	startMenu menu(window);
+
 	sf::Event event;
-		  Clock clock;
-	while (window.isOpen())
-	{
-		
-		float time = clock.getElapsedTime().asMicroseconds() / 800;
-		clock.restart();
-		while (window.pollEvent(event))
-		{
-			// Close window : exit
-			if (event.type == sf::Event::Closed)
-				window.close();
+	  Clock clock;
+	  while (window.isOpen())
+	  {
 
-			if (event.type == Event::KeyPressed) {
-				if (event.key.code == Keyboard::D) {
-						
-				}
-			}
-		}
-		//menu.menuControl();
-		//menu.runMenu();
-		
-		window.clear();
-		
-		ExplosionAnimation->update(time);
-		ExplosionAnimation->draw(150.f, 150.f, window);
-		window.display();
-=======
+		  float time = clock.getElapsedTime().asMicroseconds() / 800.f;
+		  clock.restart();
+		  while (window.pollEvent(event))
+		  {
+			  // Close window : exit
+			  if (event.type == sf::Event::Closed)
+				  window.close();
 
-	/*new Game().run();*/
-	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
-			   // все ,что ниже - только для теста
+			  if (event.type == Event::KeyPressed) {
+				  if (event.key.code == Keyboard::D) {
 
+				  }
+			  }
+		  }
 
-//	startMenu menu(window);
-	pauseMenu  pauseMenu(window);
-	while (window.isOpen())
-	{
-			
-		
-//		menu.runMenu();
-		pauseMenu.runMenu();
->>>>>>> master
-	}							  
+		  //menu.runMenu();
 
+		  window.clear();
+
+		  ExplosionAnimation->update(time);
+		  ExplosionAnimation->draw(150.f, 150.f, window);
+		  window.display();
+	  }
 	return 0;
 }
