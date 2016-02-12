f03e0bb0654a9c3c02b035d4c44ab9fce20bda9c
DarKRs <centralstreet@mail.ru>
2016-02-12 12:03:05 +0300

    Враги №2
    Небольшой ИИ
    
    Signed-off-by: DarKRs

--- Enemy/Enemy.cpp
+++ Enemy/Enemy.cpp
@@ -1,5 +1,5 @@
 #include "Enemy.h"
-
+#include "Hero.h"
 using namespace sf;
 
 //Коментарии к функциям есть в Enemy.h
@@ -59,6 +59,14 @@ Vector2f Enemy::getEnemyPossition(){
 	return E_X_Y_Poss;
 }
 
+float Enemy::getEnemyPossitionX(){
+	return E_X_Y_Poss.x;
+}
+
+float Enemy::getEnemyPossitionY(){
+	return E_X_Y_Poss.y;
+}
+
 int Enemy::getEnemyVectorMove(){
 	return E_DX;
 }
@@ -69,9 +77,109 @@ int Enemy::getEnemyVectorMoveLast(){
 
 ////////////////////////////////////////////////////
 
+void Enemy::EnemyGoingHero(){
+	/*	if (getHeroPossitionY() < getEnemyPossitionY()){ //Если герой находится выше врага
+	//Поиск по карте объектов ladder(лестницы или лифта или еще чего нибудь что его может поднять)
+	}
+	if (getHeroPossitionY > getEnemyPossitionY()){
+	//Поиск по карте объектов которые спустят врага на уровень с героем. Если таковых способов нет надо прерывать функцию
+	}
+	if (getHeroPossitionX() < getEnemyPossitionX) //Если герой находится левее врага
+	{
+	EnemyShoot();
+	E_Speed = -3; //Якобы заставляем врага идти в лево со скоростью 3
+	E_DX = 2; //Вектор движения будет равнятся 4 т.к идет в лево
+	E_DXL = 2; //И ставит вектор прошедшего движения
+	}
+	if (getHeroPossitionX() > getEnemyPossitionX){ //Если герой находится правее врага
+	EnemyShoot();
+	E_Speed = 3; //Заставляем идти врага в право со скоростью 3
+	E_DX = 1; //Вектора движения ставим на 2 т.к идем в право
+	E_DXL = 1;
+	}*/
+}
+
+void Enemy::EnemyIdle(){
+	//Небольшое патрулирование или что то типо того
+}
+
+void Enemy::SeeEnemy(){
+	if (Если врага видит){
+		E_SeeEnemy = true;
+		if (Если растояние удовлетворяет растоянию стрельбы){
+			E_CanAttack = true;
+		}
+		else if (Если растояние неудовлетворяет растоянию стрельбы){
+			E_CanAttack = false;
+		}
+	}
+	if (Если врага не видит){
+		E_SeeEnemy = false;
+		E_CanAttack = false;
+	}
+}
+
+void Enemy::EnemyControl(){
+	if (E_SeeEnemy && E_CanAttack){
+		EnemyShoot();
+		//Возможно движение в сторону героя
+	}
+	else if (E_SeeEnemy && !E_CanAttack){
+		EnemyGoingHero();
+		E_Alert = true;
+		E_AlertIntevalFloat = 100;
+	}
+	else if (!E_SeeEnemy && E_Alert){
+		EnemyGoingHero();
+		E_AlertIntevalFloat--;
+		if (E_AlertIntevalFloat < 0){
+			E_Alert=false
+		}
+	}
+	else if (!E_SeeEnemy){
+		EnemyIdle();
+	}
+	
+	
+}
+
+void Enemy::EnemyShoot(){
+	//Реализация стрельбы
+}
+
+void Enemy::EnemyDraw(RenderWindow &window){
+	window.draw(E_Animation);
+}
+
+void Enemy::Gravity(){
+	if (!E_onGround){
+		//Смешение спрайта вниз по Y со скоростью 
+	}
+}
+
+void Enemy::CollisionEnemy(){
+	/*for (int i = 0; i < objGround.size(); i++)
+	{
+		if (H_Animation.getGlobalBounds().intersects(objGround[i].rect))
+		{
+			H_onGround = true;
+			moveHero(0, -0.5*time.asMilliseconds());// Отталкиваем от земли
+		}
+	}
+	for (int i = 0; i < objSolid.size(); i++)
+	{
+		if (H_Animation.getGlobalBounds().intersects(objSolid[i].rect))
+		{
+			if (H_DX == 2) { moveHero(-H_Speed* time.asMilliseconds(), 0); } // от боковых сторон 
+			if (H_DX == 4) { moveHero(H_Speed *time.asMilliseconds(), 0); }
+		}
+	}*/
+}
+
 void Enemy::updateAndDrawEnemy(Time &time, RenderWindow &window){
 	//window.draw();
-
-	//ИИ
-	//Если E_Act=false то не атаковать
+	Gravity();
+	SeeEnemy();
+	CollisionEnemy();
+	EnemyControl();
 }
--- Enemy/Enemy.h
+++ Enemy/Enemy.h
@@ -17,19 +17,35 @@ public:
 	int getEnemySpeed(); //Взять текущее значение скорости врага
 	int getEnemyAttack(); //Взять текущее значение урона врага
 	Vector2f getEnemyPossition(); //Взять текущее положение по X , Y
+	float getEnemyPossitionX(); //Взять положение по X
+	float getEnemyPossitionY(); //Взять положение по Y
 	int getEnemyVectorMove();  // Тукущий вектор движения(если не движется то он будет 0, если движется то темод будет возвражять 1 если в верх, 2 если в право, 3 если вниз, 4 если в лево)
 	int getEnemyVectorMoveLast();  //Вектор движнния в прошедшем времени служит для определиния анимации если стоит 
 	////////Прочее////////////
-	void updateAndDrawEnemy(Time &time, RenderWindow &window);//Отрисовка и движение (Заставляет жить)
+	void EnemyGoingHero(); //Преследование врагом героя
+	void EnemyIdle(); //Состояние врага в спокойном положении
+	void SeeEnemy(); //Проверка видит ли он врага
+	void EnemyControl(); //ИИ врага
+	void EnemyShoot(); //Реализация стрельбы врага
+	void EnemyDraw(RenderWindow &window); //Отрисовка врага вызываемая в функции updateAndDrawEnemy
+	void Gravity(); //Воздействие гравитации на врага
+	void CollisionEnemy(); //Функция с столкновением врага со всем что его окружает
+	void updateAndDrawEnemy(Time &time, RenderWindow &window);//Отрисовка и вызов остальных функций типо  ИИ и т.д
 private:
 	bool E_onGround;								 // На замле или нет
 	bool E_Act;										//Активен ли враг		
+	bool E_SeeEnemy;								//Видит ли он героя
+	bool E_CanAttack;								//Может ли атаковать
+	bool E_Alert;									//Встревожен ли враг. Нужно для того что бы враг сразу не терял интерес к герою когда враг перестанет видить героя
 	int E_Hp;                                      // Жизни
 	int E_Arm;                                    // Броня
 	int E_Attack;								 //Урон наносимый врагом
-	int E_DX;                                    //Вектор движения
+	int E_DX;                                    //Вектор движения 1-вправо, 2 -влево.
 	int E_DXL;                                  // Вектор прошедшего движения
+	float E_AlertIntevalFloat;					//Время которое будет указыватся в конструкторе для E_Alert
 	float E_Speed;                             // Cкорость персонажа  
 	Vector2f E_X_Y_Poss;                   // Позиция по х и у
 	String E_Name;                        // Имя
+	Time E_AlertInteval;					//Таймер для E_Alert
+	Animation E_Animation;				  //Класс анимации
 };
