16926b405e1369ccdd2bf9467c0573996d43ba45
Alexey <alekseydavaydosvidaniya@gmail.com>
2016-02-12 22:53:51 +1000

    simple push

--- main.cpp
+++ main.cpp
@@ -1,24 +1,24 @@
 #include <SFML/Graphics.hpp>
 #include "MainGameClass/Game.h"
-#include "Screens/StartMenu/StartMenu.h"
+#include "Screens/Menu/StartMenu.h"
+#include "Screens/Menu/pauseMenu.h"
 int main()
 {
 	
-<<<<<<< HEAD
+
 	/*new Game().run();*/
 	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
 			   // все ,что ниже - только для теста
-=======
-	/* new Game().run(); */
-	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
-			   // все ,что ниже - только для теста !!!-!
->>>>>>> ScreenAndManager
-	startMenu menu(window);
+
+
+//	startMenu menu(window);
+	pauseMenu  pauseMenu(window);
 	while (window.isOpen())
 	{
 			
-		menu.menuControl();
-		menu.runMenu();
+		
+//		menu.runMenu();
+		pauseMenu.runMenu();
 	}							  
 
 	return 0;
